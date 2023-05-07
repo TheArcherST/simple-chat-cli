f9ddc14db72de27a2453dfa5d360eb92848bb0d9
Shapovalov Mihail <mihailsapovalov05@gmail.com>
2023-05-07 22:30:19 +0300

    Fixup

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setuptools.setup(name="simple_chat_client", version="1.0",
                  url='#',
                  author='Shapovalov Mihail',
                  author_email='mihailsapovalov05@gmail.com',
-                 entry_points={"console_scripts": ["simple_chat_client = simple_chat_client.ui:cli"]},
+                 entry_points={"console_scripts": ["simple_chat_cli = simple_chat_client.ui:cli"]},
                  zip_safe=False)

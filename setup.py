954ac1ee6e9aeb1d854a0afc4a647f78c971c0fe
Shapovalov Mihail <mihailsapovalov05@gmail.com>
2023-05-08 00:24:26 +0300

    Fixup

--- setup.py
+++ setup.py
@@ -2,7 +2,16 @@ import setuptools
 
 
 setuptools.setup(name="simple_chat_client", version="1.0",
-                 install_requires=open('requirements.txt').read().splitlines(),
+                 packages=[
+                    'simple_chat_client',
+                 ],
+                 install_requires=[
+                    'requests~=2.26.0',
+                    'dataclass_factory',
+                    'textual~=0.23.0',
+                    'typing_extensions',
+                    'aiohttp~=3.7.4.post0',
+                 ],
                  url='#',
                  author='Shapovalov Mihail',
                  author_email='mihailsapovalov05@gmail.com',

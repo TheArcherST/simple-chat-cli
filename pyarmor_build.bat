83a88692e345599d22c29d4643f203b54b6f9eb3
Shapovalov Mihail <mihailsapovalov05@gmail.com>
2023-05-14 14:03:08 +0300

    Resolve build conflict
    It must be possible to build with or without obfuscation. To achieve it, was created two separated setup configs. In instructions of building with pyarmor was included swapping of default file to pyarmor's version, and rolling changes back.
    
    Also, fixed problems with automatically depencies install.

--- pyarmor_build.bat
+++ pyarmor_build.bat
@@ -1,3 +1,7 @@
 ./venv/bin/pip install pyarmor.cli.runtime~=3.2.0
-./venv/bin/pyarmor gen --recursive --platform windows.x86_64 --platform darwin.x86_64 --platform darwin.arm64 --platform linux.x86_64 -i simple_chat_client/
+./venv/bin/pyarmor gen --recursive --platform windows.x86_64 --platform darwin.x86_64 --platform darwin.arm64 --platform linux.x86_64 -i simple_chat_client
+mv setup.cfg plain_setup.cfg
+mv pyarmor_setup.cfg setup.cfg
 ./venv/bin/python -m build . --skip-dependency-check --no-isolation
+mv setup.cfg pyarmor_setup.cfg
+mv plain_setup.cfg setup.cfg
